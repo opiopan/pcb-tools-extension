0d91ed834d7a28b249b86081294a9c1b95e8f39c
opiopan <opiopan@gmail.com>
2019-04-07 23:32:03 +0900

    compliant with PyPi repository

--- setup.py
+++ setup.py
@@ -19,15 +19,15 @@ METADATA = {
     'url': "http://github.com/opiopan/pcb-tools-extension",
     'packages': ['gerberex'],
     'long_description': read('README.md'),
-    'classifiers':[
+    'long_description_content_type': 'text/markdown', 
+    'classifiers': [
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language:: Python:: 2",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 }
 
